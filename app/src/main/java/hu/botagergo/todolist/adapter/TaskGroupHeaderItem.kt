0878b38161df8f1a95059a38a74bbd4d28748001
Bóta Gergely <botagergo@gmail.com>
2021-09-28 16:10:20 +0200

    Added expand/collapse icon to task groups

--- app/src/main/java/hu/botagergo/todolist/adapter/TaskGroupHeaderItem.kt
+++ app/src/main/java/hu/botagergo/todolist/adapter/TaskGroupHeaderItem.kt
@@ -1,5 +1,8 @@
 package hu.botagergo.todolist.adapter
 
+import android.content.Context
+import android.graphics.drawable.Drawable
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.recyclerview.widget.ItemTouchHelper
 import com.xwray.groupie.ExpandableGroup
 import com.xwray.groupie.ExpandableItem
@@ -9,18 +12,27 @@ import hu.botagergo.todolist.R
 import kotlinx.android.synthetic.main.item_task_group_header.view.*
 
 
-class TaskGroupHeaderItem(val groupName: String) : Item(), ExpandableItem {
+class TaskGroupHeaderItem(private val groupName: String) : Item(), ExpandableItem {
 
     private lateinit var expandableGroup: ExpandableGroup
+
 
     override fun setExpandableGroup(onToggleListener: ExpandableGroup) {
         this.expandableGroup = onToggleListener
     }
 
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+
+        viewHolder.root.button_groupName.setCompoundDrawables(
+            getIcon(viewHolder.root.context, expandableGroup.isExpanded), null, null, null
+        )
+
         viewHolder.root.button_groupName.text = groupName
         viewHolder.root.button_groupName.setOnClickListener {
-            this.expandableGroup.onToggleExpanded()
+            expandableGroup.onToggleExpanded()
+            viewHolder.root.button_groupName.setCompoundDrawables(
+                getIcon(viewHolder.root.context, expandableGroup.isExpanded), null, null, null
+            )
         }
     }
 
@@ -29,5 +41,17 @@ class TaskGroupHeaderItem(val groupName: String) : Item(), ExpandableItem {
     }
 
     override fun getLayout() = R.layout.item_task_group_header
+
+    private fun getIcon(context: Context, isExpanded: Boolean) : Drawable? {
+        val drawable = getDrawable(
+            context,
+            if (isExpanded)
+                R.drawable.ic_expanded
+            else
+                R.drawable.ic_collapsed
+        )
+        drawable?.setBounds(0, 0, 60, 60)
+        return drawable
+    }
 
 }
